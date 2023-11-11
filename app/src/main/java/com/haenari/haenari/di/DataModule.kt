f4809109570b3d9695e9a4549b87001b33e7d3c0
osemin <semin015123@gmail.com>
2023-11-11 15:08:01 +0900

    add(DataModule) : add di for weather repo

--- app/src/main/java/com/haenari/haenari/di/DataModule.kt
+++ app/src/main/java/com/haenari/haenari/di/DataModule.kt
@@ -1,8 +1,15 @@
 package com.haenari.haenari.di
 
+import androidx.datastore.core.DataStore
+import com.haenari.haenari.Weather.WeatherData
 import com.haenari.haenari.data.api.RetrofitCreator
 import com.haenari.haenari.data.api.RetrofitProvider
 import com.haenari.haenari.data.api.WeatherAPI
+import com.haenari.haenari.data.dao.WeatherDao
+import com.haenari.haenari.data.repository.weather.WeatherRepositoryImpl
+import com.haenari.haenari.data.repository.weather.source.WeatherLocalDataSource
+import com.haenari.haenari.data.repository.weather.source.WeatherRemoteDataSource
+import com.haenari.haenari.domain.repository.weather.WeatherRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,4 +26,26 @@ class DataModule {
     @Singleton
     @Provides
     fun providesWeatherAPI(): WeatherAPI = providesRetrofitCreator().weatherAPI()
+
+    /**
+     * Weather
+     */
+    @Singleton
+    @Provides
+    fun providesWeatherRemoteDataSource(api: WeatherAPI) = WeatherRemoteDataSource(api)
+
+    @Singleton
+    @Provides
+    fun providesWeatherLocalDataSource(
+        protoDataStore: DataStore<WeatherData>,
+        dao: WeatherDao
+    ): WeatherLocalDataSource = WeatherLocalDataSource(protoDataStore, dao)
+
+    @Singleton
+    @Provides
+    fun providesWeatherRepository(
+        localDataSource: WeatherLocalDataSource,
+        remoteDataSource: WeatherRemoteDataSource
+    ): WeatherRepository =
+        WeatherRepositoryImpl(localDataSource, remoteDataSource)
 }

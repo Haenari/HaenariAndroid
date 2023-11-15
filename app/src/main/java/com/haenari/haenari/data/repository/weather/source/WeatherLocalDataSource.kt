deba05bc6aaf44773fcea27775f9dcb26707d513
osemin <semin015123@gmail.com>
2023-11-15 20:12:53 +0900

    fix(WeatherLocalDataSource) : add inject constructor

--- app/src/main/java/com/haenari/haenari/data/repository/weather/source/WeatherLocalDataSource.kt
+++ app/src/main/java/com/haenari/haenari/data/repository/weather/source/WeatherLocalDataSource.kt
@@ -3,10 +3,11 @@ package com.haenari.haenari.data.repository.weather.source
 import androidx.datastore.core.DataStore
 import com.haenari.haenari.Weather.WeatherData
 import com.haenari.haenari.data.dao.WeatherDao
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class WeatherLocalDataSource(
+class WeatherLocalDataSource @Inject constructor(
     private val protoDataStore: DataStore<WeatherData>,
     private val dao: WeatherDao
 ) {

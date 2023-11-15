c03cbd2e88f3d3e510951370367c820dcc3586d1
osemin <semin015123@gmail.com>
2023-11-15 20:14:55 +0900

    fix(DataModule) : add di for data store

--- app/src/main/java/com/haenari/haenari/di/DataModule.kt
+++ app/src/main/java/com/haenari/haenari/di/DataModule.kt
@@ -1,11 +1,17 @@
 package com.haenari.haenari.di
 
+import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.haenari.haenari.Weather.WeatherData
 import com.haenari.haenari.data.api.RetrofitCreator
 import com.haenari.haenari.data.api.RetrofitProvider
 import com.haenari.haenari.data.api.WeatherAPI
 import com.haenari.haenari.data.dao.WeatherDao
+import com.haenari.haenari.data.database.HaenariDatabase
+import com.haenari.haenari.data.database.RoomDaoProvider
+import com.haenari.haenari.data.datastore.weatherPreferencesDataStore
+import com.haenari.haenari.data.datastore.weatherProtoDataStore
 import com.haenari.haenari.data.repository.weather.WeatherRepositoryImpl
 import com.haenari.haenari.data.repository.weather.source.WeatherLocalDataSource
 import com.haenari.haenari.data.repository.weather.source.WeatherRemoteDataSource
@@ -13,6 +19,7 @@ import com.haenari.haenari.domain.repository.weather.WeatherRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -26,6 +33,30 @@ class DataModule {
     @Singleton
     @Provides
     fun providesWeatherAPI(): WeatherAPI = providesRetrofitCreator().weatherAPI()
+
+    @Singleton
+    @Provides
+    fun providesRoomDaoProvider(@ApplicationContext context: Context): RoomDaoProvider =
+        HaenariDatabase.create(context)
+
+    @Singleton
+    @Provides
+    fun providesWeatherDao(roomDaoProvider: RoomDaoProvider): WeatherDao =
+        roomDaoProvider.weatherDao()
+
+    @Singleton
+    @Provides
+    fun providesWeatherPreferencesDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> =
+        context.weatherPreferencesDataStore
+
+    @Singleton
+    @Provides
+    fun providesWeatherProtoDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<WeatherData> =
+        context.weatherProtoDataStore
 
     /**
      * Weather

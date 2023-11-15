b1410f5d3fb4d3b582cc1176e20be347e5700202
osemin <semin015123@gmail.com>
2023-11-15 20:08:29 +0900

    fix(Plugin) : downgrade kotlin version

--- buildSrc/src/main/kotlin/Plugin.kt
+++ buildSrc/src/main/kotlin/Plugin.kt
@@ -10,7 +10,7 @@ object Plugin {
     object Version {
         const val application = "8.1.1"
         const val library = "8.1.1"
-        const val kotlin = "1.9.0"
+        const val kotlin = "1.8.22"
         const val hilt = "2.48.1"
         const val protobuf = "0.9.3"
         const val protoc = "3.25.0"

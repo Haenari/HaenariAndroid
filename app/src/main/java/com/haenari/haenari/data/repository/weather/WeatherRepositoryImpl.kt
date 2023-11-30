d65315cb361c0ca950f362599cfbf7b31e7c0055
osemin <semin015123@gmail.com>
2023-11-30 22:10:33 +0900

    add(WeatherRepository) : add function

--- app/src/main/java/com/haenari/haenari/data/repository/weather/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/haenari/haenari/data/repository/weather/WeatherRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.haenari.haenari.data.repository.weather
 
+import com.haenari.haenari.data.entity.WeatherEntity
 import com.haenari.haenari.data.repository.weather.source.WeatherLocalDataSource
 import com.haenari.haenari.data.repository.weather.source.WeatherRemoteDataSource
+import com.haenari.haenari.data.util.checkIsSuccess
 import com.haenari.haenari.domain.repository.weather.WeatherRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +12,33 @@ import javax.inject.Singleton
 class WeatherRepositoryImpl @Inject constructor(
     private val localDataSource: WeatherLocalDataSource,
     private val remoteDataSource: WeatherRemoteDataSource
-): WeatherRepository {
+) : WeatherRepository {
+    // todo api 별로 read & local update + caching time -> result = Boolean
+    // todo 실제 사용할 데이터 꺼내올 함수 따로 만들기
 
+    override suspend fun syncShortTerm(date: String, time: String, nx: Int, ny: Int): Boolean {
+        val response = remoteDataSource.getShortTerm(date, time, nx, ny)
+        return response.checkIsSuccess { entity ->
+            localDataSource.updateShortTerm(entity.getItems())
+        }
+    }
+
+    override suspend fun syncMidTermLand(regId: String, tmFc: String): Boolean {
+        val response = remoteDataSource.getMidTermLand(regId, tmFc)
+        return response.checkIsSuccess { entity ->
+            localDataSource.updateMidTermLand(entity)
+        }
+    }
+
+    override suspend fun syncMidTermTemperature(regId: String, tmFc: String): Boolean {
+        val response = remoteDataSource.getMidTermTemperature(regId, tmFc)
+        return response.checkIsSuccess { entity ->
+            localDataSource.updateMidTermTemperature(entity)
+        }
+    }
+
+    //todo for test
+    override fun readAllWeather(): List<WeatherEntity> {
+        return localDataSource.readAllWeather()
+    }
 }
--- app/src/main/java/com/haenari/haenari/domain/repository/weather/WeatherRepository.kt
+++ app/src/main/java/com/haenari/haenari/domain/repository/weather/WeatherRepository.kt
@@ -1,4 +1,15 @@
 package com.haenari.haenari.domain.repository.weather
 
+import com.haenari.haenari.data.entity.WeatherEntity
+
 interface WeatherRepository {
+    suspend fun syncShortTerm(date: String, time: String, nx: Int, ny: Int): Boolean
+
+    suspend fun syncMidTermLand(regId: String, tmFc: String): Boolean
+
+    suspend fun syncMidTermTemperature(regId: String, tmFc: String): Boolean
+
+    // todo for test
+    fun readAllWeather(): List<WeatherEntity>
+
 }

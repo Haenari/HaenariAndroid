0614636b4f1e12c2f06ea49a5224ff9fee4ddef8
osemin <semin015123@gmail.com>
2023-11-30 22:08:39 +0900

    add(WeatherEntity) : add typeConverter and defaultValue function

--- app/src/main/java/com/haenari/haenari/data/entity/WeatherEntity.kt
+++ app/src/main/java/com/haenari/haenari/data/entity/WeatherEntity.kt
@@ -1,16 +1,29 @@
 package com.haenari.haenari.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.haenari.haenari.data.dao.WeatherTypeConverter
 import com.haenari.haenari.data.database.HaenariDatabase
 
-@Entity(
-    tableName = HaenariDatabase.WEATHER_TABLE
-)
+@Entity(tableName = HaenariDatabase.WEATHER_TABLE)
+@TypeConverters(WeatherTypeConverter::class)
 data class WeatherEntity(
     @PrimaryKey val date: String,
-    val minTemperature: Int,
-    val maxTemperature: Int,
+    @ColumnInfo val value: List<Weather>
+)
+
+data class Weather(
+    val temperature: Float,
     val skyStatus: String,
     val probabilityOfPrecipitation: Int
-)
+) {
+    companion object {
+        fun defaultValue() = Weather(
+            Float.MAX_VALUE,
+            "",
+            Int.MAX_VALUE
+        )
+    }
+}

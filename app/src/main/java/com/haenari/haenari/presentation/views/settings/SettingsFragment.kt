d4304b3589b95585fdd6d0ab1c86a478206f88b7
osemin <semin015123@gmail.com>
2023-12-09 16:40:35 +0900

    add(SettingsFragment) : add webview and version name

--- app/src/main/java/com/haenari/haenari/presentation/views/settings/SettingsFragment.kt
+++ app/src/main/java/com/haenari/haenari/presentation/views/settings/SettingsFragment.kt
@@ -1,8 +1,11 @@
 package com.haenari.haenari.presentation.views.settings
 
+import android.content.Intent
+import com.haenari.haenari.BuildConfig
 import com.haenari.haenari.R
 import com.haenari.haenari.databinding.FragmentSettingsBinding
 import com.haenari.haenari.presentation.base.fragment.BindFragment
+import com.haenari.haenari.presentation.views.webview.WebViewActivity
 
 class SettingsFragment : BindFragment<FragmentSettingsBinding>(R.layout.fragment_settings) {
 
@@ -10,6 +13,28 @@ class SettingsFragment : BindFragment<FragmentSettingsBinding>(R.layout.fragment
         fun newInstance() = SettingsFragment()
     }
     override fun initView() {
+        bind {
+            tvVersion.text = getString(R.string.version_info, BuildConfig.VERSION_NAME)
 
+            tvPrivacyPolicy.setOnClickListener {
+                context?.let {
+                    val intent = Intent(it, WebViewActivity::class.java).apply {
+                        putExtra(WebViewActivity.EXTRA_LOAD_URL, BuildConfig.URL_PRIVACY_POLICY)
+                        putExtra(WebViewActivity.EXTRA_TITLE, getString(R.string.privacy_policy))
+                    }
+                    startActivity(intent)
+                }
+            }
+
+            tvTermsOfService.setOnClickListener {
+                context?.let {
+                    val intent = Intent(it, WebViewActivity::class.java).apply {
+                        putExtra(WebViewActivity.EXTRA_LOAD_URL, BuildConfig.URL_TERMS_AND_CONDITIONS)
+                        putExtra(WebViewActivity.EXTRA_TITLE, getString(R.string.terms_of_service))
+                    }
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }

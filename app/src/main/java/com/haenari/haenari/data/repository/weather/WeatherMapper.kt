464bc1430fe0316a4f5076efd1ff0a21605fc1df
osemin <semin015123@gmail.com>
2023-12-09 16:08:21 +0900

    fix(WeatherMapper) : fix mapper

--- app/src/main/java/com/haenari/haenari/data/repository/weather/WeatherMapper.kt
+++ app/src/main/java/com/haenari/haenari/data/repository/weather/WeatherMapper.kt
@@ -1,10 +1,19 @@
 package com.haenari.haenari.data.repository.weather
 
+import com.haenari.haenari.LegacyUtil.handleAmPm
 import com.haenari.haenari.data.entity.Weather
 import com.haenari.haenari.data.entity.WeatherEntity
 import com.haenari.haenari.data.model.weather.ShortTermItem
+import java.lang.Integer.max
 
 object WeatherMapper {
+
+    private const val MAX_TEMPERATURE = "TMX"
+    private const val MIN_TEMPERATURE = "TMN"
+    private const val PRECIPITATION_TYPE = "PTY"
+    private const val PROBABILITY_OF_PRECIPITATION = "POP"
+    private const val SKY_STATUS = "SKY"
+    private const val TEMPERATURE = "TMP"
 
     fun List<ShortTermItem>.toWeatherEntity(): List<WeatherEntity> {
         return this.groupBy {
@@ -14,37 +23,124 @@ object WeatherMapper {
             val list = it.value.groupBy { it.fcstTime }
 
             date to list
-        }.map { firstMap ->
-            val date = checkNotNull(firstMap.first)
+        }.map { dateMap ->
+            val date = checkNotNull(dateMap.first)
             val mutableList = MutableList(24) { Weather.defaultValue() }
+            var maxTemperature = Float.MAX_VALUE
+            var minTemperature = Float.MAX_VALUE
+            var weatherAM = Int.MAX_VALUE to Int.MAX_VALUE
+            var weatherPM = Int.MAX_VALUE to Int.MAX_VALUE
+            var skyStatusAM = Int.MAX_VALUE
+            var skyStatusPM = Int.MAX_VALUE
+            var precipitationTypeAM = Int.MAX_VALUE
+            var precipitationTypePM = Int.MAX_VALUE
+            var probabilityOfPrecipitationAM = Int.MIN_VALUE
+            var probabilityOfPrecipitationPM = Int.MIN_VALUE
 
-            firstMap.second.map { secondMap ->
-                val time = secondMap.key
-                val index = checkNotNull(time).toInt() / 100
-                val list = secondMap.value
+            dateMap.second.map { timeMap ->
+                val time = timeMap.key
+                val hour = checkNotNull(time).toInt() / 100
+                val list = timeMap.value
 
                 var temperature = Float.MAX_VALUE
-                var skyStatus = ""
+                var skyStatus = Int.MAX_VALUE
+                var precipitationType = Int.MAX_VALUE
                 var probabilityOfPrecipitation = Int.MAX_VALUE
                 list.map {
                     when (it.category) {
-                        "VVV" -> {
+                        TEMPERATURE -> {
                             temperature = it.fcstValue?.toFloat() ?: Float.MAX_VALUE
                         }
 
-                        "REH" -> {
+                        PROBABILITY_OF_PRECIPITATION -> {
                             probabilityOfPrecipitation = it.fcstValue?.toInt() ?: Int.MAX_VALUE
+
+                            hour.handleAmPm(
+                                onAM = {
+                                    probabilityOfPrecipitationAM = max(
+                                        probabilityOfPrecipitationAM,
+                                        probabilityOfPrecipitation
+                                    )
+                                },
+                                onPM = {
+                                    probabilityOfPrecipitationPM = max(
+                                        probabilityOfPrecipitationPM,
+                                        probabilityOfPrecipitation
+                                    )
+                                }
+                            )
                         }
 
-                        "PTY" -> {
-                            skyStatus = it.fcstValue ?: ""
+                        PRECIPITATION_TYPE -> {
+                            precipitationType = it.fcstValue?.toInt() ?: Int.MAX_VALUE
+
+                            hour.handleAmPm(
+                                onAM = {
+                                    precipitationTypeAM = checkPrecipitationTypePriority(
+                                        precipitationTypeAM,
+                                        precipitationType
+                                    )
+                                },
+                                onPM = {
+                                    precipitationTypePM = checkPrecipitationTypePriority(
+                                        precipitationTypePM,
+                                        precipitationType
+                                    )
+                                }
+                            )
+                        }
+
+                        SKY_STATUS -> {
+                            skyStatus = it.fcstValue?.toInt() ?: Int.MAX_VALUE
+
+                            hour.handleAmPm(
+                                onAM = {
+                                    skyStatusAM = checkSkyStatusPriority(skyStatusAM, skyStatus)
+                                },
+                                onPM = {
+                                    skyStatusPM = checkSkyStatusPriority(skyStatusPM, skyStatus)
+                                }
+                            )
+                        }
+
+                        MAX_TEMPERATURE -> {
+                            maxTemperature = it.fcstValue?.toFloat() ?: Float.MAX_VALUE
+                        }
+
+                        MIN_TEMPERATURE -> {
+                            minTemperature = it.fcstValue?.toFloat() ?: Float.MAX_VALUE
                         }
                     }
                 }
-                mutableList[index] = Weather(temperature, skyStatus, probabilityOfPrecipitation)
+                weatherAM = skyStatusAM to precipitationTypeAM
+                weatherPM = skyStatusPM to precipitationTypePM
+                mutableList[hour] = Weather(
+                    temperature = temperature,
+                    skyStatus = skyStatus,
+                    probabilityOfPrecipitation = probabilityOfPrecipitation,
+                    precipitationType = precipitationType
+                )
             }
-            //Logs.e("result::${WeatherEntity(date, mutableList)}")
-            WeatherEntity(date = date, value = mutableList)
+            WeatherEntity(
+                date = date,
+                minTemperature = minTemperature,
+                maxTemperature = maxTemperature,
+                probabilityOfPrecipitationAM = probabilityOfPrecipitationAM,
+                probabilityOfPrecipitationPM = probabilityOfPrecipitationPM,
+                weatherAM = weatherAM,
+                weatherPM = weatherPM,
+                value = mutableList,
+            )
         }
+    }
+
+    private fun checkPrecipitationTypePriority(oldValue: Int, newValue: Int): Int {
+        // todo check priority
+        return newValue
+    }
+
+    private fun checkSkyStatusPriority(oldValue: Int, newValue: Int): Int {
+        // todo check priority
+        return newValue
     }
 }

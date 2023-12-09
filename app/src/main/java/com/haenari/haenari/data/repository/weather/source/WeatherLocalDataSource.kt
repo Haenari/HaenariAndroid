ed00e01f717a68199ad5900137e0d560ce744362
osemin <semin015123@gmail.com>
2023-12-09 16:08:42 +0900

    fix(WeatherLocalDataSource) : add mapper

--- app/src/main/java/com/haenari/haenari/data/repository/weather/source/WeatherLocalDataSource.kt
+++ app/src/main/java/com/haenari/haenari/data/repository/weather/source/WeatherLocalDataSource.kt
@@ -6,11 +6,11 @@ import androidx.datastore.preferences.core.edit
 import com.haenari.haenari.Weather.WeatherData
 import com.haenari.haenari.data.dao.WeatherDao
 import com.haenari.haenari.data.datastore.PreferencesKey
-import com.haenari.haenari.data.entity.Weather
 import com.haenari.haenari.data.entity.WeatherEntity
 import com.haenari.haenari.data.model.weather.MidTermLandEntity
 import com.haenari.haenari.data.model.weather.MidTermTemperatureEntity
 import com.haenari.haenari.data.model.weather.ShortTermItem
+import com.haenari.haenari.data.repository.weather.WeatherMapper.toWeatherEntity
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -40,45 +40,7 @@ class WeatherLocalDataSource @Inject constructor(
     }
 
     fun updateShortTerm(items: List<ShortTermItem>) {
-        val result = items.groupBy {
-            it.fcstDate
-        }.map {
-            val date = it.key
-            val list = it.value.groupBy { it.fcstTime }
-
-            date to list
-        }.map { firstMap ->
-            val date = checkNotNull(firstMap.first)
-            val mutableList = MutableList(24) { Weather.defaultValue() }
-
-            firstMap.second.map { secondMap ->
-                val time = secondMap.key
-                val index = checkNotNull(time).toInt() / 100
-                val list = secondMap.value
-
-                var temperature = Float.MAX_VALUE
-                var skyStatus = ""
-                var probabilityOfPrecipitation = Int.MAX_VALUE
-                list.map {
-                    when (it.category) {
-                        "VVV" -> {
-                            temperature = it.fcstValue?.toFloat() ?: Float.MAX_VALUE
-                        }
-
-                        "REH" -> {
-                            probabilityOfPrecipitation = it.fcstValue?.toInt() ?: Int.MAX_VALUE
-                        }
-
-                        "PTY" -> {
-                            skyStatus = it.fcstValue ?: ""
-                        }
-                    }
-                }
-                mutableList[index] = Weather(temperature, skyStatus, probabilityOfPrecipitation)
-            }
-            //Logs.e("result::${WeatherEntity(date, mutableList)}")
-            WeatherEntity(date, mutableList)
-        }
+        val result = items.toWeatherEntity()
 
         dao.insertWeatherList(result)
     }

6c29644d75d3bb066c831c6c2374d8d8b7b83982
osemin <semin015123@gmail.com>
2023-12-25 17:12:35 +0900

    add(DateTimes) : add getMidTerm Weather String

--- app/src/main/java/com/haenari/haenari/presentation/util/DateTimes.kt
+++ app/src/main/java/com/haenari/haenari/presentation/util/DateTimes.kt
@@ -5,5 +5,15 @@ import org.joda.time.DateTime
 object DateTimes {
     fun DateTime.date(): String = this.toString("yyyyMMdd")
 
-    fun time(dateTime: DateTime): String = dateTime.toString("HHmm")
+    fun DateTime.time(): String = this.toString("HHmm")
+
+    fun midTermWeatherTime(): String {
+        return if (DateTime.now().hourOfDay >= 18) {
+            DateTime.now().withTimeAtStartOfDay().plusHours(18).toString("yyyyMMddHHmm")
+        } else if (DateTime.now().hourOfDay >= 6) {
+            DateTime.now().withTimeAtStartOfDay().plusHours(6).toString("yyyyMMddHHmm")
+        } else {
+            DateTime.now().withTimeAtStartOfDay().minusHours(6).toString("yyyyMMddHHmm")
+        }
+    }
 }

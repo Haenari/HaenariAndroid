2911cc749b6304f290430eade0339ed894cf18cb
osemin <semin015123@gmail.com>
2024-01-15 20:51:33 +0900

    fix(SyncMidTermUseCase) : add flow

--- app/src/main/java/com/haenari/haenari/domain/usecase/weather/SyncMidTermTemperatureUseCase.kt
+++ app/src/main/java/com/haenari/haenari/domain/usecase/weather/SyncMidTermTemperatureUseCase.kt
@@ -1,13 +1,17 @@
 package com.haenari.haenari.domain.usecase.weather
 
 import com.haenari.haenari.domain.repository.weather.WeatherRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class SyncMidTermTemperatureUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository
-): suspend (String, String) -> Boolean {
-    override suspend fun invoke(regId: String, tmFc: String): Boolean {
+): suspend (String, String) -> Flow<Boolean> {
+    override suspend fun invoke(regId: String, tmFc: String): Flow<Boolean> {
         // todo recursion
-        return weatherRepository.syncMidTermTemperature(regId = regId, tmFc = tmFc)
+        return flow {
+            emit(weatherRepository.syncMidTermTemperature(regId = regId, tmFc = tmFc))
+        }
     }
 }

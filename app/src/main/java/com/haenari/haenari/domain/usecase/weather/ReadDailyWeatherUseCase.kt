a75cebd5929d0f7dadd5dee6e14c5bacd66085c4
osemin <semin015123@gmail.com>
2024-01-15 20:50:26 +0900

    fix(ReadDailyWeatherUseCase) : add flow

--- app/src/main/java/com/haenari/haenari/domain/usecase/weather/ReadDailyWeatherUseCase.kt
+++ app/src/main/java/com/haenari/haenari/domain/usecase/weather/ReadDailyWeatherUseCase.kt
@@ -2,12 +2,16 @@ package com.haenari.haenari.domain.usecase.weather
 
 import com.haenari.haenari.data.entity.WeatherEntity
 import com.haenari.haenari.domain.repository.weather.WeatherRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ReadDailyWeatherUseCase @Inject constructor(
     private val weatherRepository: WeatherRepository
-) : suspend (String) -> WeatherEntity {
-    override suspend fun invoke(date: String): WeatherEntity {
-        return weatherRepository.getDailyWeather(date)
+) : suspend (String) -> Flow<WeatherEntity> {
+    override suspend fun invoke(date: String): Flow<WeatherEntity> {
+        return flow {
+            emit(weatherRepository.getDailyWeather(date))
+        }
     }
 }

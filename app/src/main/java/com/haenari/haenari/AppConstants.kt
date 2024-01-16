01300129260299d568b99c7b7fc4978344791bb8
osemin <semin015123@gmail.com>
2024-01-16 20:19:06 +0900

    fix(AppConstants) : add default address

--- app/src/main/java/com/haenari/haenari/AppConstants.kt
+++ app/src/main/java/com/haenari/haenari/AppConstants.kt
@@ -2,7 +2,7 @@ package com.haenari.haenari
 
 object AppConstants {
 
-    const val CACHING_TIME = 0
+    const val CACHING_TIME = 30
 
-    const val DEFAULT_ADDRESS = ""
+    const val DEFAULT_ADDRESS = "서울 종로구 사직로 161 경복궁"
 }

9283173f0b29b89cb6ec3f3bcf603e33332efbc8
osemin <semin015123@gmail.com>
2024-02-03 18:15:43 +0900

    fix(WeatherFragment.kt) : add item decoration

--- app/src/main/java/com/haenari/haenari/presentation/views/weather/WeatherFragment.kt
+++ app/src/main/java/com/haenari/haenari/presentation/views/weather/WeatherFragment.kt
@@ -1,9 +1,14 @@
 package com.haenari.haenari.presentation.views.weather
 
+import android.graphics.Rect
+import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.haenari.haenari.R
 import com.haenari.haenari.databinding.FragmentWeatherBinding
 import com.haenari.haenari.presentation.base.fragment.MVIFragment
+import com.haenari.haenari.presentation.util.Dimensions
 import com.haenari.haenari.presentation.util.Logs
 import com.haenari.haenari.presentation.util.Permissions
 import com.haenari.haenari.presentation.views.main.MainActivity
@@ -22,6 +27,7 @@ class WeatherFragment :
 
     override fun render(state: WeatherState) {
         Logs.e("WeatherFragment::render::$state")
+        Logs.e("WeatherFragment::render::weatherList::${state.weeklyWeather}")
         bind {
             with (state) {
                 tvLocation.text = address
@@ -36,8 +42,39 @@ class WeatherFragment :
     override fun initView() {
         bind {
             rvWeather.adapter = recyclerAdapter
+            rvWeather.addItemDecoration(object : ItemDecoration(){
+                override fun getItemOffsets(
+                    outRect: Rect,
+                    view: View,
+                    parent: RecyclerView,
+                    state: RecyclerView.State
+                ) {
+                    val startIndex = 0
+                    val endIndex = recyclerAdapter.itemCount.minus(1)
+                    context?.let {
+                        when (parent.getChildAdapterPosition(view)) {
+                            startIndex -> {
+                                outRect.top = Dimensions.dpToPx(it, 8)
+                                outRect.bottom = Dimensions.dpToPx(it, 4)
+                            }
 
-            btnCurrentLocation.setOnClickListener {
+                            endIndex -> {
+                                outRect.top = Dimensions.dpToPx(it, 4)
+                                outRect.bottom = Dimensions.dpToPx(it, 4)
+                            }
+
+                            else -> {
+                                outRect.top = Dimensions.dpToPx(it, 4)
+                                outRect.bottom = Dimensions.dpToPx(it, 8)
+                            }
+                        }
+                    }
+
+
+                }
+            })
+
+            ibCurrentLocation.setOnClickListener {
                 Permissions.requestLocationPermission(
                     onPermissionGranted = {
                         (activity as MainActivity).requestLocation()
